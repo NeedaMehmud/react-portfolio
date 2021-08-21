a4a17ade495111b9b22a6810debfd1326c49ead9
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-20 20:34:29 -0400

    added links to footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import SocialFollow from "./SocialFollow"
+import SocialFollow from "./components/SocialFollow"
 
 export default function App() {
   return (
--- src/components/SocialFollow.js
+++ src/components/SocialFollow.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faGithub,
+    faTwitter,
+    faLinkedinIn
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function SocialFollow() {
-  return (
-    <div class="social-container">
-      <h3>Social Follow</h3>
-    </div>
-  );
+    return (
+        <div class="social-container">
+            <h3>Social Follow</h3>
+            <a href="https://www.github.com/NeedaMehmud2017/"
+                className="github social">
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+            <a href="https://www.twitter.com/needa_mehmud/"
+                className="twitter social">
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+            <a href="https://www.linkedin.com/in/needamehmud/"
+                className="linkedin social">
+                <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+            </a>
+        </div>
+    );
 }
+
