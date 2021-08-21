c93e6fdcb02ec9f7bcbaf24417be83ec64a6be77
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-20 23:24:50 -0400

    comment out footer

--- src/components/SocialFollow.js
+++ src/components/SocialFollow.js
@@ -1,29 +1,29 @@
-import React from "react";
-import { AppCss } from "../App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faGithub,
-    faTwitter,
-    faLinkedinIn
-} from "@fortawesome/free-brands-svg-icons";
+// import React from "react";
+// import { AppCss } from "../App.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//     faGithub,
+//     faTwitter,
+//     faLinkedinIn
+// } from "@fortawesome/free-brands-svg-icons";
 
-export default function SocialFollow() {
-    return (
-        <div className="bg-gray flex-wrap md:mb-0">
-                    <br></br>
-            <a href="https://www.github.com/NeedaMehmud2017/"
-                className="github social">
-                <FontAwesomeIcon icon={faGithub} size="2x" />
-            </a>
-            <a href="https://www.twitter.com/needa_mehmud/"
-                className="twitter social">
-                <FontAwesomeIcon icon={faTwitter} size="2x" />
-            </a>
-            <a href="https://www.linkedin.com/in/needamehmud/"
-                className="linkedin social">
-                <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
-            </a>
-        </div>
-    );
-}
+// export default function SocialFollow() {
+//     return (
+//         <div className="bg-gray flex-wrap md:mb-0">
+//                     <br></br>
+//             <a href="https://www.github.com/NeedaMehmud2017/"
+//                 className="github social">
+//                 <FontAwesomeIcon icon={faGithub} size="2x" />
+//             </a>
+//             <a href="https://www.twitter.com/needa_mehmud/"
+//                 className="twitter social">
+//                 <FontAwesomeIcon icon={faTwitter} size="2x" />
+//             </a>
+//             <a href="https://www.linkedin.com/in/needamehmud/"
+//                 className="linkedin social">
+//                 <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+//             </a>
+//         </div>
+//     );
+// }
 
